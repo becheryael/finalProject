e6c01e5e2fc70712cd336658a9b170e05a22994d
YaelyB <becher.yael@gmail.com>
2026-01-03 21:03:17 +0200

    added name and personalnum

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,12 @@
 import mongoose, { Document } from "mongoose";
 import jwt from "jsonwebtoken";
+import validator from "validator";
 import bcrypt from "bcrypt";
 import { NextFunction } from "express";
 
 export interface UserType extends Document {
+  name: string;
+  personalNum: string;
   email: string;
   manager: boolean;
   password: string;
@@ -17,11 +20,32 @@ interface UserModel extends mongoose.Model<UserType> {
 }
 
 const UserSchema = new mongoose.Schema<UserType>({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  personalNum: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+    validate(value: string) {
+      if (Number.isNaN(value) || value.length !== 7) {
+        throw new Error('Must contain seven digits');
+      }
+    },
+  },
   email: {
     type: String,
     required: true,
     trim: true,
     unique: true,
+    validate(value: string) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Must contain an email.');
+      }
+    },
   },
   manager: {
     type: Boolean,
@@ -33,8 +57,8 @@ const UserSchema = new mongoose.Schema<UserType>({
     minLength: 7,
     trim: true,
     validate(value: string) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error('Password cannot contain "Password".');
+      if (value.length < 7) {
+        throw new Error('Password must contain at least 7 characters.');
       }
     },
   },
@@ -81,7 +105,7 @@ UserSchema.statics.findByCredentials = async (email, password) => {
   if (password == user.password) {
     isMatch = true;
   } else {
-    isMatch = false
+    isMatch = false;
   }
 
   if (!isMatch) {
